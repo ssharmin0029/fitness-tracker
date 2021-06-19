21433eed686bebabc8978635254d939501f09c9b
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-06-19 02:52:12 -0400

    htmlRoutes -> style.css fixed

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,6 +1,30 @@
 const router = require('express').Router();
 const Workout = require('../../models');
 
+// // Add new exercises to a new workout plan.
+// router.post('/', ({body}, res) => {
+//     Workout.create(body)
+//     .then(newWorkout => {
+//         res.json(newWorkout);
+//     })
+//     .catch(err => {
+//         res.status(400).json(err);
+//     });
+// });
 
+// // Add exercises to the most recent workout plan.
+// router.put('/:id', ({params, body}, res) => {
+//     Workout.findOneAndUpdate(
+//         {_id: params.id},
+//         {$push: {exercises: body}},
+//         {upsert: true, useFindandModify: false}
+//     )
+//     .then(newWorkout => {
+//         res.json(newWorkout);
+//     })
+//     .catch(err => {
+//         res.status(400).json(err);
+//     });
+// });
 
 module.exports = router;
