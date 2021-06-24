ebace01ea19111a185a6d889a364c903a474dba7
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-06-23 22:47:24 -0400

    fixed workoutRoutes so can access model

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,30 +1,44 @@
 const router = require('express').Router();
-const Workout = require('../../models');
+const Workout = require('../../models/workout.js');
 
-// // Add new exercises to a new workout plan.
-// router.post('/', ({body}, res) => {
-//     Workout.create(body)
-//     .then(newWorkout => {
-//         res.json(newWorkout);
-//     })
-//     .catch(err => {
-//         res.status(400).json(err);
-//     });
-// });
+router.get('/', (req, res) => {
+    console.log(Workout)
+    Workout.find({})
+    .then(newWorkout => {
+        res.json(newWorkout);
+    }) 
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
-// // Add exercises to the most recent workout plan.
-// router.put('/:id', ({params, body}, res) => {
-//     Workout.findOneAndUpdate(
-//         {_id: params.id},
-//         {$push: {exercises: body}},
-//         {upsert: true, useFindandModify: false}
-//     )
-//     .then(newWorkout => {
-//         res.json(newWorkout);
-//     })
-//     .catch(err => {
-//         res.status(400).json(err);
-//     });
-// });
+// View the combined weight of multiple exercises from the past seven workouts on the `stats` page.
+// View the total duration of each workout from the past seven workouts on the `stats` page.
+
+// Add new exercises to a new workout plan.
+router.post('/', ({body}, res) => {
+    Workout.create(body)
+    .then(newExercises => {
+        res.json(newExercises);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+// Add exercises to the most recent workout plan.
+router.put('/:id', ({params, body}, res) => {
+    Workout.findOneAndUpdate(
+        {_id: params.id},
+        {$push: {exercises: body}},
+        {upsert: true, useFindandModify: false}
+    )
+    .then(newExercise => {
+        res.json(newExercise);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 module.exports = router;
