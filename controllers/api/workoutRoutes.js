83c5b9c4aa0a4bd7e248ecb915bf12fdaf437fe2
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-06-24 01:41:22 -0400

    workoutRoutes.js updated

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -45,6 +45,7 @@ router.put('/:id', ({params, body}, res) => {
     Workout.findOneAndUpdate(
         {_id: params.id},
         {$push: {exercises: body}},
+        {upsert: true, useFindandModify: false}
     )
     .then(newExercise => {
         res.json(newExercise);
